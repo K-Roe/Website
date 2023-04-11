e70f348446dfc83320a667a5f184f7c49410551a
K-Roe <karlroe919286@msn.com>
2023-04-11 22:42:11 +0100

    working on the display

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,21 @@
-@extends('components.layout')
+<x-layout>
+@include('_posts-header')
 
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/blog/{{ $post->slug }}">
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
+        @if ($posts->count() > 1)
+        <div class="lg:grid lg:grid-cols-2">
+           @foreach($posts->skip(1) as $post)
+                <x-post-card :post="$post"/>
+            @endforeach
+        </div>
+            @endif
+        @else
+        <p class="text-center">No posts yet. Please check back later</p>
+        @endif
+    </main>
 
-                    {!! $post->title !!}
 
-                </a>
-            </h1>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
 
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
-
-@endsection
-
+</x-layout>
